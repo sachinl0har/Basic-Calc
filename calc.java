706a5a805e9f7c6c9f4d35a2fa6a2c7b786e0652
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-05-18 11:27:38 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -5,9 +5,9 @@ public class calc {
         double a, b, c, e = 0, x, y, z;
         int d;
         Scanner calc = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");  
+        System.out.print("\033[H\033[2J");
         while(e == 0){
-            System.out.println("\n1. Addition + \n2. Subtraction - \n3. Multipication * \n4. Division / \n5. Modulus % \n6. Hypotenuse \n7. Power of a Number \n8. Random Number \n9. Converter \n10. Armstrong Number \n11. Reverse a Number \n12. Exit \nEnter Your Choice: \n");
+            System.out.println("\n1. Addition + \n2. Subtraction - \n3. Multipication * \n4. Division / \n5. Modulus % \n6. Hypotenuse \n7. Power of a Number \n8. Random Number \n9. Converter \n10. Armstrong Number \n11. Reverse a Number \n12. Circle \n13. Rectangle \n14. Exit \nEnter Your Choice: \n");
         d = calc.nextInt();
         switch(d){
             case 1:
@@ -149,6 +149,90 @@ public class calc {
                 System.out.println("Reversed Number of " + rnum + " is " + rev);
                 break;
             case 12:
+                System.out.print("\033[H\033[2J");
+                Scanner circle = new Scanner(System.in);
+                double radius, circumference, area, diameter;
+                int choice;
+                System.out.println("Circle\n1. Radius \n2. Diameter \n3. Circumference \n4. Area \nEnter Your Choice: ");
+                choice = circle.nextInt();
+                switch(choice){
+                    case 1:
+                        System.out.println("Enter Circumference of Circle: ");
+                        circumference = circle.nextDouble();
+                        radius = circumference/(2 * 3.14);
+                        System.out.println("Radius of Circumference " + circumference + " = " + radius);
+                        break;
+                    case 2:
+                        System.out.println("Enter Radius of Circle: ");
+                        radius = circle.nextDouble();
+                        diameter = 2 * radius;
+                        System.out.println("The Diameter of radius " + radius + " Circle = " + diameter);
+                        break;
+                    case 3:
+                        System.out.println("Enter Radius of Circle: ");
+                        radius = circle.nextDouble();
+                        circumference = 2 * 3.14 * radius;
+                        System.out.println("The Circumference of Radius " + radius + " Circle = " + circumference);
+                        break;
+                    case 4:
+                        System.out.println("Enter Radius of Circle: ");
+                        radius = circle.nextDouble();
+                        area = 3.14 * radius * radius;
+                        System.out.println("The Area of Radius " + radius + " Circle = " + area);
+                        break;
+                }
+                break;
+            case 13:
+                System.out.print("\033[H\033[2J");
+                double length, width, rectangleArea, perimeter, diagonal;
+                int option1;
+                Scanner rectangle = new Scanner(System.in);
+                System.out.println("Rectangle\n1. Area \n2. Diagonal \n3. Perimeter \n4. Length \n5. Width \nEnter Your Choice: ");
+                option1 = rectangle.nextInt();
+                switch(option1){
+                    case 1:
+                        System.out.println("Enter Length of the Rectangle: ");
+                        length = rectangle.nextDouble();
+                        System.out.println("Enter Width of the Rectangle: ");
+                        width = rectangle.nextDouble();
+                        rectangleArea = length * width;
+                        System.out.println("The Area of the Rectangle " + length + " Length " + width + " Width is " + rectangleArea);
+                        break;
+                    case 2:
+                        System.out.println("Enter Length of the Rectangle: ");
+                        length = rectangle.nextDouble();
+                        System.out.println("Enter Width of the Rectangle: ");
+                        width = rectangle.nextDouble();
+                        diagonal = Math.sqrt((length * length) + (width * width));
+                        System.out.println("The Diagonal of the Rectangle " + length + " Length " + width + " width is " + diagonal);
+                        break;
+                    case 3:
+                        System.out.println("Enter Length of the Rectangle: ");
+                        length = rectangle.nextDouble();
+                        System.out.println("Enter Width of the Rectangle: ");
+                        width = rectangle.nextDouble();
+                        perimeter = 2 * (length + width);
+                        System.out.println("The Perimeter of the Rectangle " + length + " Length " + width + " width is " + perimeter);
+                        break;
+                    case 4:
+                        System.out.println("Enter Width of the Rectangle: ");
+                        width = rectangle.nextDouble();
+                        System.out.println("Enter Perimeter of the Rectangle: ");
+                        perimeter = rectangle.nextDouble();
+                        length = (perimeter/2) - width;
+                        System.out.println("The Length of Rectangle whose Perimeter = " + perimeter + " and Width = " + width + " is " + length);
+                        break;
+                    case 5:
+                        System.out.println("Enter Length of the Rectangle: ");
+                        length = rectangle.nextDouble();
+                        System.out.println("Enter Perimeter of the Rectangle: ");
+                        perimeter = rectangle.nextDouble();
+                        width = (perimeter/2) - length;
+                        System.out.println("The Width of Rectangle whose Perimeter = " + perimeter + " and Length = " + length + " is " + width);
+                        break;
+                }
+                break;
+            case 14:
                 System.out.print("\033[H\033[2J");
                 e = 1;
                 System.out.println("\nThank You For Visiting\n");
